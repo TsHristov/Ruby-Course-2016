c8d81a99c57725797e339d001df2e8bb4fec64d9
TsHristov <tsvetanhristov@yahoo.com>
2016-10-30 19:30:21 +0200

    fix recursion in Hash#reshape

--- Tasks/02/solution.rb
+++ Tasks/02/solution.rb
@@ -5,7 +5,7 @@ class Array
   end
 
   def reshape(shape)
-    self.map { |element| element.reshape(shape) }
+    map { |element| element.reshape(shape) }
   end
 end
 
@@ -14,13 +14,17 @@ class Hash
     keys = keys.split('.') if keys.is_a? String
     key = keys.first.to_sym if self.keys.include? keys.first.to_sym
     key = keys.first if self.keys.include? keys.first
+    return nil unless key
     return self[key] if keys.size == 1
     self[key].fetch_deep(keys[1..keys.size])
   end
 
-  def reshape(other)
+  def reshape(shape)
     reshaped = {}
-    other.each { |key, value| reshaped[key] = self.fetch_deep(value) }
+    shape.each do |key, value|
+      if value.is_a? String then reshaped[key] = self.fetch_deep(value) end
+      if value.is_a? Hash then reshaped[key] = self.reshape(value) end
+    end
     reshaped
   end
 end
